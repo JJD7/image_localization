e53bdd55113c1967f703cb059d73e12b8d676eef
James Donnelly <jd7@vt.edu>
2018-11-01 22:04:27 -0400

    updated convolution loop

--- untitled.m
+++ untitled.m
@@ -5,20 +5,20 @@ img = imread('map/keas1.png');
 test_img = imread('onboard_images/frame001378.png');
 test_img = imresize(test_img, 0.1);
 F = fft2(test_img);
-H = conj(fft2(img));
+H = fft2(img);
 
 G = zeros(size(img));
 kx = 128;
 ky = 72;
 
-for i=1:length(G(1,:,1))-kx
-    for j=1:length(G(:,1,1))-ky
-        H_subset = H(j:ky,i:kx,:);
+for i=1:length(G(1,:,1))-kx:kx
+    for j=1:length(G(:,1,1))-ky:ky
+        H_subset = conj(H(j:j+ky-1,i:i+kx-1,:));
         col = i+kx-1;
         row = j+ky-1;
-        G_test = F.*H_subset;
+        G(j:row,i:col,:) = F.*H_subset;
     end
 end
 
-G_space = ifft2(G);
+G_space = ifft(G);
 imshow(G)
